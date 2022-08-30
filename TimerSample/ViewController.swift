49eb6af235940d0aa2d272a9ace27877ef9c07a7
hasanuysaal <hsnuysaal@icloud.com>
2022-08-30 11:54:17 +0300

    first commit

--- TimerSample/ViewController.swift
+++ TimerSample/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeText: UITextField!
+    
+    var remainTime = 0
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        timeLabel.text = "Kalan zaman: \(remainTime)"
+        
     }
 
-
+    
+    @IBAction func chooseTime(_ sender: Any) {
+        
+        remainTime = Int(timeText.text ?? "") ?? 0
+        timeLabel.text = "Kalan zaman: \(remainTime)"
+        
+    }
+    
+    @IBAction func startBtn(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func timerFunc(){
+        
+        if remainTime > 0 {
+            
+            remainTime = remainTime - 1
+            timeLabel.text = "Kalan zaman: \(remainTime)"
+            
+        } else {
+            
+            timeLabel.text = "Süre Bitti"
+            timer.invalidate()
+            
+        }
+    }
 }
 
